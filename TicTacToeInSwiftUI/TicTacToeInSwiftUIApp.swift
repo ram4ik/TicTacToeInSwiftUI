d76e2a52170c74acd47f7d6820b7933f90f45be0
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-01-17 10:58:51 +0200

    Initial Commit

---
+++ TicTacToeInSwiftUI/TicTacToeInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeInSwiftUIApp.swift
+//  TicTacToeInSwiftUI
+//
+//  Created by ramil on 17.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
